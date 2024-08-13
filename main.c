40867121df2706ba94e1f4a98bd3575615e6fe19
Miguel Brondani <brondani.miguel@gmail.com>
2024-08-13 16:22:34 -0300

    chave hash, conferindo colisoes

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int calculoCnpj(char* cnpj){
         ascii = cnpj[i];
         soma = (soma + ascii);
     }
-    return soma % 17931362;
+    return soma;
 }
 
 int calculoNome(char* linha){
@@ -22,12 +22,22 @@ int calculoNome(char* linha){
         soma += linha[i];
         i++;
     }
-    return soma % 17931362;
+    return soma;
+}
+
+int verifica_repeticao(int* array, int tamanho, int numero) {
+    int i;
+    for (i = 0; i < tamanho; i++) {
+        if (array[i] == numero) {
+            return 1; // número encontrado
+        }
+    }
+    return 0; // número não encontrado
 }
 
 int main(){
     //FILE* arq = fopen("C:\\Users\\Miguel\\Documents\\Aulas\\POD\\trab2\\Empresas0\\K3241.K03200Y0.D40713.EMPRECSV", "r");
-    FILE* arq = fopen("C:\\Users\\lucas\\Desktop\\POD\\buscaHash\\K3241.K03200Y0.D40713.EMPRECSV", "r");
+    FILE* arq = fopen("Empresas0\\K3241.K03200Y0.D40713.EMPRECSV", "r");
 
     if(arq == NULL) {
         printf("Erro ao abrir o arquivo.\n");
@@ -38,22 +48,41 @@ int main(){
     int max = 0;
     char cnpj[9];
     
-    while(feof(arq) != true && max != 50){
+	// !!!RETIRAR!!!
+	int qtdParada =200000;
+    int chaves[qtdParada];
+    int qtdColi = 0, vetChave=0;
+    
+    while(feof(arq) != true && max != qtdParada){
         fgets(row, MAXCHAR, arq);
-        printf("%s\n", row);
+//        printf("%s\n", row);
         strncpy(cnpj, row+1, 8);
         cnpj[8] = '\0';
         
-        int somaCnpj = calculoCnpj(cnpj);
+        int somaCnpj = calculoCnpj(cnpj)*100000;
         int somaNome = calculoNome(row);
         
-        printf("CNPJ extraido: '%s'\n", cnpj);
-        printf("Soma CNPJ: %d\n", somaCnpj);
-        printf("Soma Nome: %d\n", somaNome);
+        int chave = somaCnpj + somaNome;
         
+//        printf("CNPJ extraido: '%s'\n", cnpj);
+//        printf("Soma CNPJ: %d\n", somaCnpj);
+//        printf("Soma Nome: %d\n", somaNome);
+//        printf("Chave Hash: %d\n", chave);
+
+        
+		
+		if(verifica_repeticao(chaves, vetChave, chave)==1){
+			printf("!!!!COLISAO PARA %d\n", chave);
+			qtdColi++;
+		}
+		chaves[vetChave] = chave;
+        vetChave++;
+		
         max++;
     }
 
     fclose(arq);
+    
+    printf("\n\nHOUVERAM %d COLISOES", qtdColi);
     return 0;
 }
